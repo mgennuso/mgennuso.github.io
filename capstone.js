ce49a9173252b87264dac0bc9bb4da475aae492f
mgennuso <127543961+mgennuso@users.noreply.github.com>
2024-01-25 13:38:45 -0500

    Update capstone.js

--- capstone.js
+++ capstone.js
@@ -1,3 +1,12 @@
 function myFunction() {
   alert("You've Subscribed to Recipe Library!");
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
